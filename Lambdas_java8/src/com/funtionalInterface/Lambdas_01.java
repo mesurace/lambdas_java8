ed7210fbeec8dcbef4cd4368b11713eb02a5abf5
Suresh Adhikari <mesuraceer@gmail.com>
2017-06-03 00:34:50 -0700

    Added Consumer

--- Lambdas_java8/src/com/funtionalInterface/Lambdas_01.java
+++ Lambdas_java8/src/com/funtionalInterface/Lambdas_01.java
@@ -1,5 +1,6 @@
 package com.funtionalInterface;
 
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /**
@@ -11,11 +12,13 @@ public class Lambdas_01 {
 	public static void main(String[] args) {
 
 		// retuns true if input string is "Apples" otherwise false
-		Predicate<String> stringLen = (s) -> s.equals("Apples");
-		System.out.println(stringLen.test("Apples"));
-		System.out.println(stringLen.test("Oranges"));
+		Predicate<String> stringCompare = (s) -> s.equals("Apples");
+		System.out.println(stringCompare.test("Apples"));
+		System.out.println(stringCompare.test("Oranges"));
 		
-		
+		//one input no output
+		Consumer<String> consumer=(s)-> System.out.println(s.toLowerCase());
+		consumer.accept("Java 8-Functional Interfaces-Consumer");
 		
 	}
 
