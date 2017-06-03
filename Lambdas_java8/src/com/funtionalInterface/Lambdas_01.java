a16aaf879fb6b9492f8f287cceaca52be2b9c81e
Suresh Adhikari <mesuraceer@gmail.com>
2017-06-03 00:50:30 -0700

    Added unary operator

--- Lambdas_java8/src/com/funtionalInterface/Lambdas_01.java
+++ Lambdas_java8/src/com/funtionalInterface/Lambdas_01.java
@@ -5,6 +5,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 /**
  * @author Suresh Adhikari
@@ -35,7 +36,9 @@ public class Lambdas_01 {
 		BinaryOperator<Integer> add= (a,b)->a+b;
 		System.out.println(add.apply(5, 5));
 		
-		
+		//example of unary operator
+		UnaryOperator<String> string=(s)->s.toUpperCase();
+		System.out.println(string.apply("hi!!"));
 		
 	}
 
